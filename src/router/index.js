f9a6ce621561e11daf1c7eabc058e8794cd80a0b
rakafebriansy <kerenr445@gmail.com>
2024-09-29 22:57:11 +0700

    chore: setting up react navigation

---
+++ src/router/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from '../pages/Home/index'
+
+const Stack = createNativeStackNavigator();
+
+const Router = () => {
+    return (
+        <Stack.Navigator>
+            <Stack.Screen name="Home" component={HomeScreen} options={{ headerShown: false }}/>
+        </Stack.Navigator>
+    )
+}
+
+export default Router;
